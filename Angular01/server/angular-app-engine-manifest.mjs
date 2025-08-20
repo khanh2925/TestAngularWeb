d20667bcdfc510b502dfc88a089681e52b595e55
Quoc Khanh <23641111.khanh@student.iuh.edu.vn>
2025-08-20 11:17:18 +0700

    quametmoi

--- Angular01/server/angular-app-engine-manifest.mjs
+++ Angular01/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/Angular01',
+  basePath: '/TestAngularWeb',
   supportedLocales: {
   "en-US": ""
 },
--- Angular01/server/angular-app-manifest.mjs
+++ Angular01/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/Angular01/',
+  baseHref: '/TestAngularWeb/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/Angular01"
+    "route": "/TestAngularWeb"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 444, hash: 'bb2f7a13d0754fb15339bc9ea592c83e9fe220d1405d54ac7a7db157aae6d9af', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 957, hash: '83dae2a3f1cd71908c49e531950b6dcd70ad3e68ec796263edfee4c9bd5ec6a5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 894, hash: 'f2b7208acfe5978e5d52b05eff35a44413c0449ab4d3b3877570dd0325cca130', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 449, hash: '09954ae4fe2493c0607f9c5ab37a314d4a0bb5a4f3a6e4d8bcb48e72f171ec4c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 962, hash: '1b05f51a32184609152f590c689a12997aeee583a3f581c66884ed1dee5a20f9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 899, hash: '44caec144cd44c92ece2473c1fcb41e94925bd581b14eb9408245db13a6e464b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
